7f31e2012db6c162e50952a675faa0a62cbb9141
drqrack <chrisappia461@gmail.com>
2025-09-05 14:01:03 +0000

    changes

--- sections/hero.py
+++ sections/hero.py
@@ -4,10 +4,10 @@ from nicegui import ui
 def render():
    
     # Big container for all contents
-    with ui.element("div").style("background-image: url('/assets/welcome.jpg')").classes("h-screen w-screen flex flex-col bg-cover bg-center justify-center items-center"):
+    with ui.element("div").style("background-image: url('/assets/hero.jpg')").classes("h-screen w-screen flex flex-col bg-cover bg-center justify-center items-center"):
         
         with ui.element("nav").classes("flex flex row justify-between items-center w-full fixed left-0 top-0 px-20 py-10"):
-            ui.label("LOGO").classes("text-white font-bold text-2xl")
+            ui.label("LOGO").classes("text-orange-400 font-bold text-2xl")
 
             navlinks = [{"title": "Home", "path": "/"}, 
                         {"title": "Menu", "path": "/"}, 
@@ -27,8 +27,8 @@ def render():
                 ui.html('<i class="fa-brands fa-twitter"></i>')
 
         with ui.element("div").classes("text-white font-bold text-center"):
-            ui.label("Welcome to").classes("text-4xl")
-            ui.html("<h1>Family's Studio<h1>").classes("text-7xl")
+            ui.label("Welcome to").classes("text-4xl mb-4")
+            ui.label("Family's Studio").classes("text-8xl text-orange-500 mb-8")
             ui.button("Look Menu").props("color=red").classes("")
 
                 

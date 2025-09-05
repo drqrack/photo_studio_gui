24c801f683b32cb16310d89f80610511686d1abd
drqrack <chrisappia461@gmail.com>
2025-09-05 15:32:00 +0000

    new changges

--- sections/hero.py
+++ sections/hero.py
@@ -7,7 +7,7 @@ def render():
     with ui.element("div").style("background-image: url('/assets/hero.jpg')").classes("h-screen w-screen flex flex-col bg-cover bg-center justify-center items-center"):
         
         with ui.element("nav").classes("flex flex row justify-between items-center w-full fixed left-0 top-0 px-20 py-10"):
-            ui.label("LOGO").classes("text-orange-400 font-bold text-2xl")
+            ui.label("f-S").classes("text-orange-400 font-bold text-lg hover:text-white cursor-pointer bg-green-900 rounded-[100%] p-2")
 
             navlinks = [{"title": "Home", "path": "/"}, 
                         {"title": "Menu", "path": "/"}, 
@@ -21,15 +21,15 @@ def render():
                 for item in navlinks:
                     ui.link(item["title"], item["path"]).classes("no-underline uppercase text-white")
 
-            with ui.row().classes("text-white"):
-                ui.html('<i class="fa-brands fa-facebook"></i>')
-                ui.html('<i class="fa-brands fa-instagram"></i>')
-                ui.html('<i class="fa-brands fa-twitter"></i>')
+            with ui.row().classes("text-orange-400 text-lg"):
+                ui.html('<i class="fa-brands fa-facebook"></i>').classes("hover:text-white cursor-pointer")
+                ui.html('<i class="fa-brands fa-instagram"></i>').classes("hover:text-white cursor-pointer")
+                ui.html('<i class="fa-brands fa-twitter"></i>').classes("hover:text-white cursor-pointer")
 
         with ui.element("div").classes("text-white font-bold text-center"):
-            ui.label("Welcome to").classes("text-4xl mb-4")
+            ui.label("Welcome to").classes("text-5xl mb-4")
             ui.label("Family's Studio").classes("text-8xl text-orange-500 mb-8")
-            ui.button("Look Menu").props("color=red").classes("")
+            ui.button("Look Menu").props("color=black-4").classes("shadow-2xl shadow-green-500")
 
                 
             

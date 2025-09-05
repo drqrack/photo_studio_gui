2adcdf69587bb33c3be27ba2dc25080a6406ee8d
drqrack <chrisappia461@gmail.com>
2025-09-05 20:36:38 +0000

    changes

--- sections/hero.py
+++ sections/hero.py
@@ -7,7 +7,7 @@ def render():
     with ui.element("div").style("background-image: url('/assets/hero.jpg')").classes("h-screen w-screen flex flex-col bg-cover bg-center justify-center items-center"):
         
         with ui.element("nav").classes("flex flex row justify-between items-center w-full fixed left-0 top-0 px-20 py-10"):
-            ui.label("m-S").classes("text-orange-400 font-bold text-lg hover:text-white cursor-pointer bg-green-900 rounded-[100%] p-2")
+            ui.label("m-S").classes("text-orange-400 font-bold text-lg hover:text-red-700 cursor-pointer bg-green-900 rounded-[100%] p-2")
 
             navlinks = [{"title": "Home", "path": "/"}, 
                         {"title": "Menu", "path": "/"}, 
@@ -21,15 +21,15 @@ def render():
                 for item in navlinks:
                     ui.link(item["title"], item["path"]).classes("no-underline uppercase text-white")
 
-            with ui.row().classes("text-orange-400 text-lg"):
-                ui.html('<i class="fa-brands fa-facebook"></i>').classes("hover:text-white cursor-pointer")
+            with ui.row().classes("text-orange-400 text-xl"):
+                ui.html('<i class="fa-brands fa-facebook"></i>').classes("text-blue-600 hover:text-white cursor-pointer")
                 ui.html('<i class="fa-brands fa-instagram"></i>').classes("hover:text-white cursor-pointer")
-                ui.html('<i class="fa-brands fa-twitter"></i>').classes("hover:text-white cursor-pointer")
+                ui.html('<i class="fa-brands fa-twitter"></i>').classes("text-sky-400 hover:text-white cursor-pointer")
 
         with ui.element("div").classes("text-white font-bold text-center"):
             ui.label("Welcome to").classes("text-5xl mb-4")
             ui.label("Family's Studio").classes("text-8xl text-orange-500 mb-8")
-            ui.button("Look Menu").props("color=black-4").classes("shadow-2xl shadow-green-500")
+            ui.button("Look Menu").props("color=black-4").classes("shadow-2xl shadow-green-500 hover:shadow-orange-700")
 
                 
             
--- sections/welcome.py
+++ sections/welcome.py
@@ -1,9 +1,12 @@
 from nicegui import ui
 
 def render():
-    with ui.element("div"):
-        with ui.element("div"):
+    with ui.element("div").classes("w-screen h-screen flex flex-row justify-center items-center"):
+        with ui.element("div").classes("w-1/2 px-10 py-10"):
             ui.label("Melinda Studio")
             ui.label("WELCOME")
             ui.label("Let's create man in our own image...we are beautiful")
             ui.link("Our Story", "/")
+
+        with ui.element("div").classes("w-1/2 px-10 py-10"):
+            ui.image("/assets/welcome.jpg").classes()
